24d751f4ac84aea005692dbb43a1dd0ac6c05391
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-03-03 19:51:45 -0500

    context refactoring

--- zmq/src/Zmq/Context.hs
+++ zmq/src/Zmq/Context.hs
@@ -7,17 +7,15 @@ module Zmq.Context
 where
 
 import Control.Exception
-import Control.Monad.Trans.Except
 import Data.Function (fix)
-import Data.Functor (void, (<&>))
 import Foreign.C.Types (CInt)
-import Libzmq qualified
+import Libzmq
 import Libzmq.Bindings qualified as Libzmq.Bindings
 import Numeric.Natural (Natural)
-import Zmq.Error (Error, enrichError, enrichFunction)
+import Zmq.Error (Error, enrichError, unexpectedError)
 
 newtype Context
-  = Context Libzmq.Zmq_ctx_t
+  = Context Zmq_ctx_t
 
 data Options = Options
   { ioThreads :: Natural,
@@ -36,40 +34,42 @@ defaultOptions =
 -- | Perform an action with a new ØMQ context.
 withContext :: Options -> (Context -> IO (Either Error a)) -> IO (Either Error a)
 withContext options action =
-  mask \restore ->
-    newContext options >>= \case
-      Left err -> pure (Left err)
-      Right context ->
-        try (restore (action context)) >>= \case
-          Left (exception :: SomeException) -> do
-            uninterruptibleMask_ (void (terminateContext context))
-            throwIO exception
-          Right result -> do
-            uninterruptibleMask_ (terminateContext context) <&> \case
-              Left err ->
-                Left case result of
-                  Left err0 -> err0 -- prefer user's error to terminate error
-                  Right _ -> err
-              Right () -> result
+  mask \restore -> do
+    context <- newContext options
+    result <- try (restore (action context))
+    uninterruptibleMask_ (terminateContext context)
+    case result of
+      Left (exception :: SomeException) -> throwIO exception
+      Right value -> pure value
 
 -- Create a new ØMQ context.
-newContext :: Options -> IO (Either Error Context)
+newContext :: Options -> IO Context
 newContext Options {ioThreads, maxMessageSize, maxSockets} = do
-  context <- Libzmq.zmq_ctx_new
-  runExceptT do
-    zmq_ctx_set context Libzmq.ZMQ_IO_THREADS (fromIntegral @Natural @Int ioThreads)
-    zmq_ctx_set context Libzmq.ZMQ_MAX_MSGSZ (fromIntegral @Natural @Int maxMessageSize)
-    zmq_ctx_set context Libzmq.ZMQ_MAX_SOCKETS (fromIntegral @Natural @Int maxSockets)
-    pure (Context context)
-  where
-    zmq_ctx_set context option value =
-      ExceptT (enrichFunction "zmq_ctx_set" (Libzmq.zmq_ctx_set context option value))
+  context <- zmq_ctx_new
+  setContextOption context ZMQ_IO_THREADS (fromIntegral @Natural @Int ioThreads)
+  setContextOption context ZMQ_MAX_MSGSZ (fromIntegral @Natural @Int maxMessageSize)
+  setContextOption context ZMQ_MAX_SOCKETS (fromIntegral @Natural @Int maxSockets)
+  pure (Context context)
 
 -- Terminate a ØMQ context.
-terminateContext :: Context -> IO (Either Error ())
+terminateContext :: Context -> IO ()
 terminateContext (Context context) = do
   fix \again ->
-    Libzmq.zmq_ctx_term context >>= \case
-      Left Libzmq.EINTR -> again
-      Left errno -> pure (Left (enrichError "zmq_ctx_term" errno))
-      Right () -> pure (Right ())
+    zmq_ctx_term context >>= \case
+      Left errno ->
+        let err = enrichError "zmq_ctx_term" errno
+         in case errno of
+              EFAULT -> throwIO err
+              EINTR -> again
+              _ -> unexpectedError err
+      Right () -> pure ()
+
+setContextOption :: Zmq_ctx_t -> Zmq_ctx_option -> Int -> IO ()
+setContextOption context option value =
+  zmq_ctx_set context option value >>= \case
+    Left errno ->
+      let err = enrichError "zmq_ctx_set" errno
+       in case errno of
+            EINVAL -> throwIO err
+            _ -> unexpectedError err
+    Right () -> pure ()
