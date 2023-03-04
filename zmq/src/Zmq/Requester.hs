d73ec6f6d5b7e4b182bac494d2dbb120a753a3c1
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-03-03 20:44:03 -0500

    add Zmq.Requester

---
+++ zmq/src/Zmq/Requester.hs
@@ -0,0 +1,51 @@
+module Zmq.Requester
+  ( Requester,
+    with,
+    bind,
+    unbind,
+    connect,
+    disconnect,
+    send,
+    receive,
+  )
+where
+
+import Data.ByteString (ByteString)
+import Data.Coerce (coerce)
+import Data.List.NonEmpty (NonEmpty)
+import Libzmq
+import Zmq.Endpoint
+import Zmq.Error (Error)
+import Zmq.Internal.Socket qualified
+
+newtype Requester
+  = Requester Zmq_socket_t
+  deriving stock (Eq)
+
+with :: (Requester -> IO (Either Error a)) -> IO (Either Error a)
+with action =
+  Zmq.Internal.Socket.with ZMQ_REQ \socket -> action (Requester socket)
+
+bind :: Requester -> Endpoint transport -> IO (Either Error ())
+bind =
+  coerce Zmq.Internal.Socket.bind
+
+unbind :: Requester -> Endpoint transport -> IO (Either Error ())
+unbind =
+  coerce Zmq.Internal.Socket.unbind
+
+connect :: Requester -> Endpoint transport -> IO (Either Error ())
+connect =
+  coerce Zmq.Internal.Socket.connect
+
+disconnect :: Requester -> Endpoint transport -> IO (Either Error ())
+disconnect =
+  coerce Zmq.Internal.Socket.disconnect
+
+send :: Requester -> NonEmpty ByteString -> IO (Either Error ())
+send =
+  coerce Zmq.Internal.Socket.send
+
+receive :: Requester -> IO (Either Error (NonEmpty ByteString))
+receive =
+  coerce Zmq.Internal.Socket.receive
