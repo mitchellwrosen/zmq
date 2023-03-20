e455a5edd2581ba4a318862b2e57040f50a6af15
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-03-20 17:31:11 -0400

    some SomeSocket instance clauses

--- zmq/src/Zmq/Internal/SomeSocket.hs
+++ zmq/src/Zmq/Internal/SomeSocket.hs
@@ -9,9 +9,12 @@ data SomeSocket
   = forall a. SomeSocket (Socket a)
 
 instance Eq SomeSocket where
-  SomeSocket x == SomeSocket y =
-    zsocket x == zsocket y
+  SomeSocket x == SomeSocket y = zsocket x == zsocket y
+  SomeSocket x /= SomeSocket y = zsocket x /= zsocket y
 
--- TODO other methods
 instance Ord SomeSocket where
   compare (SomeSocket x) (SomeSocket y) = compare (zsocket x) (zsocket y)
+  SomeSocket x < SomeSocket y = zsocket x < zsocket y
+  SomeSocket x <= SomeSocket y = zsocket x <= zsocket y
+  SomeSocket x > SomeSocket y = zsocket x > zsocket y
+  SomeSocket x >= SomeSocket y = zsocket x >= zsocket y
